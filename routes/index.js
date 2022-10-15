977cb0ee86cb9db625bbc5dcc8c0fb53cf0ec740
jeyavel-coder <96064953+jeyavel-coder@users.noreply.github.com>
2022-10-15 15:59:13 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: 'jeyavel' });
+  res.render('index', { name: 'Express' });
 });
 
 module.exports = router;
